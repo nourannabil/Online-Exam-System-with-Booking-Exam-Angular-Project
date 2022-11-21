7a9d3e8a6a99557e5685575dcd3ed957a02d0083
nouran <nourannabil12@gmail.com>
2022-11-21 13:07:34 +0300

    addComponentManageBooking

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,7 @@ import { ManageContactUsComponent } from './manage-contact-us/manage-contact-us.
 import { ManageAboutUsComponent } from './manage-about-us/manage-about-us.component';
 import { ManageHomeComponent } from './manage-home/manage-home.component';
 import { ManageTestimonialComponent } from './manage-testimonial/manage-testimonial.component';
+import { ManageBookingComponent } from './manage-booking/manage-booking.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { ManageTestimonialComponent } from './manage-testimonial/manage-testimon
     ManageContactUsComponent,
     ManageAboutUsComponent,
     ManageHomeComponent,
-    ManageTestimonialComponent
+    ManageTestimonialComponent,
+    ManageBookingComponent
   ],
   imports: [
     CommonModule,
